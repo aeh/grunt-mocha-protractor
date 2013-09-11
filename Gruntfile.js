8f6b506105441a2aff53447b039ddfb06ae71e20
Andrew Harris <andrew@noblesamurai.com>
2013-09-11 17:35:45 +1000

    test with a simplistic local copy of angularjs

--- Gruntfile.js
+++ Gruntfile.js
@@ -26,18 +26,27 @@ module.exports = function(grunt) {
         browsers: ['Chrome', 'Firefox']
       },
       files: 'test/*.js'
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 3000,
+          base: 'test/server'
+        }
+      }
     }
   });
+
+
+  require('load-grunt-tasks')(grunt);
 
   // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['mochaProtractor']);
+  grunt.registerTask('test', ['connect', 'mochaProtractor']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
